a95168a7f6e12ef6de62bbb7a8b2f692f84ec26d
kiurakku <108591564+4am-png@users.noreply.github.com>
2025-01-04 18:11:13 +0200

    Update utils.py

---
+++ src/utils.py
@@ -0,0 +1,7 @@
+import string
+
+def clean_text(text):
+    """
+    Cleans the text by removing punctuation and converting to lowercase.
+    """
+    return text.translate(str.maketrans('', '', string.punctuation)).lower()
